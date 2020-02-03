6176b34ebfbef8e978428eb524a463effcb75712
hernio <33852317+hernio@users.noreply.github.com>
2020-02-03 21:37:06 +0700

    Update w.php

--- w.php
+++ w.php
@@ -1,43 +1,23 @@
+<div class="rank-panel">
+	<div class="rank-panel-detail m-t-60">
+		<img src="<?php echo $site['img-kiri'];?>">
+		<div class="rank-title round">Website</div>
+		<img src="<?php echo $site['img-kanan'];?>">
+	</div>
+</div>
+<p>
+
 <?php
-header('Content-Type: application/json');
-
-$host = "localhost";
-$username = "id8600931_movi";
-$password = "()1234567890";
-$db_name = "id8600931_movi";
-
-$mysqli = new mysqli($host, $username, $password, $db_name);
-if(mysqli_connect_errno()) {
-echo "Error: Could not connect to database.";
-exit;
-}
+$url = 'https://cdn-k.000webhostapp.com/movi/w.php'; // path to your JSON file
+$data = file_get_contents($url); // put the contents of the file into a variable
+$characters = json_decode($data); // decode the JSON feed
+$no = 1;
 ?>
-<?php
-$query = "select * from web";
 
-//untuk menjalankan perinta sql
-$result = $mysqli->query($query);
-//untuk mendapatkan jumlah bari dari table
-$num_results = $result->num_rows;
-//cek jika data tidak 0
-if( $num_results > 0){ 
-$array = array();
-while( $row = $result->fetch_assoc() ){
-//untuk mengektrak data
-extract($row);
-$array[] = $row;
-
-}
-
-echo json_encode($array);
-
-
-}else{
-//jika data kosong maka akan menampilkan data berikutnya
-
-echo "Data Kosong";
-}
-//menutup koneksi ke database
-$result->free();
-$mysqli->close();
-?>
+<?php foreach ($characters as $character) : ?>
+    <div class="box-luar round">
+		<div class="box round <?php echo $site['tema'] ;?>"><?php echo $no++;?></div>
+		<div class="textku pd round"><?php echo $character->txt_web; ?></div>
+		<div class="mg"><a class="box kiri round <?php echo $site['btn-link'] ;?>" href="<?php echo $character->l_web; ?>">Watch</a></div>
+	</div>
+<?php endforeach; ?>
